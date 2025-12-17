ffb27614c563e5dcdbcea3202faf43940169ab7e
Muhammed Marwan <129890481+muhammedmarwan@users.noreply.github.com>
2025-12-17 18:49:45 +0530

    Revise fxmanifest.lua with new metadata and file patterns
    Updated author, description, and version fields. Refined file patterns for asset inclusion.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,20 @@
 fx_version 'cerulean'
+game 'gta5'
 
-games { 'gta5' }
+author 'Marwan'
+description 'BLAblaBLA'
+version '1.0.0'
 
 this_is_a_map 'yes'
 
-author 'your name'
-
-version '1.0.0'
-
-
-
 files {
-
-    '**/*.*'
-
+    '**/*.ymap',
+    '**/*.ytyp',
+    '**/*.ybn',
+    '**/*.ydd',
+    '**/*.ytd',
+    '**/*.yft',
+    '**/*.meta'
 }
+
+data_file 'DLC_ITYP_REQUEST' '**/*.ytyp'
